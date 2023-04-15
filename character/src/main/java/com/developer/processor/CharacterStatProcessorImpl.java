b16143aeab0b54501a66716d941a96499fddf058
5witchkr <hob4419@gmail.com>
2023-04-15 16:53:54 +0900

    feat: CharacterStatProcessor

--- character/src/main/java/com/developer/processor/CharacterStatProcessorImpl.java
+++ character/src/main/java/com/developer/processor/CharacterStatProcessorImpl.java
@@ -1,9 +1,32 @@
 package com.developer.processor;
 
+import com.developer.domainmodel.CharacterDomain;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+import java.util.UUID;
+
 public class CharacterStatProcessorImpl implements CharacterStatProcessor{
 
     @Override
-    public void updateStat(int point) {
-
+    public Mono<CharacterDomain> updateStat(CharacterDomain character, String targetStat, int point) {
+        if (targetStat.equals("backend")) {
+            return Mono.just(
+                    new CharacterDomain(character.id(),character.backend()+point,character.frontend(),character.ops(),character.health(),character.teamwork()));
+        } else if (targetStat.equals("frontend")) {
+            return Mono.just(
+                    new CharacterDomain(character.id(),character.backend(),character.frontend()+point,character.ops(),character.health(),character.teamwork()));
+        } else if (targetStat.equals("ops")) {
+            return Mono.just(
+                    new CharacterDomain(character.id(),character.backend(),character.frontend(),character.ops()+point,character.health(),character.teamwork()));
+        } else if (targetStat.equals("health")) {
+            return Mono.just(
+                    new CharacterDomain(character.id(),character.backend(),character.frontend(),character.ops(),character.health()+point,character.teamwork()));
+        } else if (targetStat.equals("teamwork")) {
+            return Mono.just(
+                    new CharacterDomain(character.id(),character.backend(),character.frontend(),character.ops(),character.health(),character.teamwork()+point));
+        } else {
+            throw new IllegalArgumentException("Not Support Stat");
+        }
     }
 }
