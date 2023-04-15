2478badb6664eb1236bac74d932aec5168058cfe
5witchkr <hob4419@gmail.com>
2023-04-15 22:50:45 +0900

    refactor: updateStat logic

--- character/src/main/java/com/developer/domainmodel/CharacterDomain.java
+++ character/src/main/java/com/developer/domainmodel/CharacterDomain.java
@@ -14,4 +14,19 @@ public record CharacterDomain(
     public CharacterDomain(UUID id){
         this(id,0,0,0 ,0,0);
     }
+    public CharacterDomain withBackend(int backend) {
+        return new CharacterDomain(this.id, backend, this.frontend, this.ops, this.health, this.teamwork);
+    }
+    public CharacterDomain withFrontend(int frontend) {
+        return new CharacterDomain(this.id, this.backend, frontend, this.ops, this.health, this.teamwork);
+    }
+    public CharacterDomain withOps(int ops) {
+        return new CharacterDomain(this.id, this.backend, this.frontend, ops, this.health, this.teamwork);
+    }
+    public CharacterDomain withHealth(int health) {
+        return new CharacterDomain(this.id, this.backend, this.frontend, this.ops, health, this.teamwork);
+    }
+    public CharacterDomain withTeamwork(int teamwork) {
+        return new CharacterDomain(this.id, this.backend, this.frontend, this.ops, this.health, teamwork);
+    }
 }
--- character/src/main/java/com/developer/processor/CharacterStatProcessorImpl.java
+++ character/src/main/java/com/developer/processor/CharacterStatProcessorImpl.java
@@ -3,30 +3,18 @@ package com.developer.processor;
 import com.developer.domainmodel.CharacterDomain;
 import reactor.core.publisher.Mono;
 
-import java.util.Objects;
-import java.util.UUID;
-
 public class CharacterStatProcessorImpl implements CharacterStatProcessor{
 
     @Override
     public Mono<CharacterDomain> updateStat(CharacterDomain character, String targetStat, int point) {
-        if (targetStat.equals("backend")) {
-            return Mono.just(
-                    new CharacterDomain(character.id(),character.backend()+point,character.frontend(),character.ops(),character.health(),character.teamwork()));
-        } else if (targetStat.equals("frontend")) {
-            return Mono.just(
-                    new CharacterDomain(character.id(),character.backend(),character.frontend()+point,character.ops(),character.health(),character.teamwork()));
-        } else if (targetStat.equals("ops")) {
-            return Mono.just(
-                    new CharacterDomain(character.id(),character.backend(),character.frontend(),character.ops()+point,character.health(),character.teamwork()));
-        } else if (targetStat.equals("health")) {
-            return Mono.just(
-                    new CharacterDomain(character.id(),character.backend(),character.frontend(),character.ops(),character.health()+point,character.teamwork()));
-        } else if (targetStat.equals("teamwork")) {
-            return Mono.just(
-                    new CharacterDomain(character.id(),character.backend(),character.frontend(),character.ops(),character.health(),character.teamwork()+point));
-        } else {
-            throw new IllegalArgumentException("Not Support Stat");
-        }
+        return Mono.just(character)
+                .map(c -> switch (targetStat) {
+                    case "backend" -> c.withBackend(c.backend() + point);
+                    case "frontend" -> c.withFrontend(c.frontend() + point);
+                    case "ops" -> c.withOps(c.ops() + point);
+                    case "health" -> c.withHealth(c.health() + point);
+                    case "teamwork" -> c.withTeamwork(c.teamwork() + point);
+                    default -> throw new IllegalArgumentException(targetStat + " Is Not Support Stat");
+                });
     }
 }
